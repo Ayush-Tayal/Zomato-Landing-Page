5e1b97a5e36d887f4bbe7cf66b44d90b91cde941
Ayush Tayal <ayushtayal1111@gmail.com>
2021-10-08 03:32:55 +0530

    update done

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
-const next = document.querySelector(".arrow-right");
-const previous = document.querySelector(".arrow-left");
-const slider = document.querySelector(".abc")
+const previous = document.querySelector(".previous");
+const next = document.querySelector(".next");
+const imageSlider = document.querySelector(".slider-container");
 const cardWidth =  document.querySelector(".dish").offsetWidth;
 
-console.log(next, previous, slider,cardWidth);
-let currentCard = 0;
 
-next.onclick=()=>{
-    console.log("Next Clicked");
-    if (currentCard < 5) {
-        currentCard++;
-        slider.style.transitionDuration = "0.5s";
-        slider.style.tarnsform = `translateX(-${currentCard * cardWidth}px)`;
+console.log(previous, previous, cardWidth, imageSlider  );
 
+let counter = 1;
+
+next.addEventListener('click', ()=>{
+    console.log("next clicked");
+    if(counter < 5){
+        imageSlider.style.transition = "transform 0.4s ease-in-out";
+        imageSlider.style.transform = 'translateX(' + (-cardWidth * counter) + 'px)';
+        counter++;
     }
-};
+});
 
-previous.onclick=()=>{
-    console.log("Previous Clicked")
-    if (currentCard < 5) {
-        currentCard--;
-        slider.style.transitionDuration = "0.5s";
-        slider.style.tarnsform = `translateX(-${currentCard * cardWidth}px)`;
-
+previous.addEventListener('click', ()=>{
+    console.log("next clicked");
+    if(counter < 5){
+        imageSlider.style.transition = "transform 0.4s ease-in-out";
+        imageSlider.style.transform = 'translateX(' + (cardWidth * counter) + 'px)';
+        counter++;
     }
-};
+});
 
+
+
+// next.onclick=()=>{
+//     console.log("Next Clicked");
+//     if (counter < 5) {
+//         counter--;
+//         slider.style.transitionDuration = "0.5s";
+//         slider.style.tarnsform = `translateX(-${counter * cardWidth}px)`;
+        
+//     }
+// };
+
+// previous.onclick=()=>{
+//     console.log("previous Clicked")
+//     if (counter < 50) {
+//         counter++;
+//         slider.style.transitionDuration = "0.5s";
+//         slider.style.tarnsform = `translateX(-${counter * cardWidth}px)`;
+        
+//     }
+// };
 const movetop = document.querySelector("#scroll-top");
 // console.log(movetop);
 
